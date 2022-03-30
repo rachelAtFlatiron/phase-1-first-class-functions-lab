9c7da9619a088970e0a8cec7bf6d915d714ef1bc
Rachel Katz <rachel.katz@flatironschool.com>
2022-03-30 14:34:22 -0400

    working solution

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
 /*~~~~~~~~~~~~~~~~~~~~MY SOLUTION~~~~~~~~~~~~~*/
+
+const drivers = ['Antonia', 'Nuru', 'Amari', 'Mo'];
+function returnFirstTwoDrivers(drivers){
+    return drivers.slice(0,2);
+}
+const returnLastTwoDrivers = (drivers) => {
+    return drivers.slice(drivers.length-2, drivers.length)
+}
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
+
+const createFareMultiplier = (num) => {
+    const retFunction = (num) => {
+        return num*num;
+    }
+    return retFunction;
+}
+
+const fareDoubler = (num) => {
+    return num*2;
+}
+
+function fareTripler(num){
+    return num*3;
+}
+
+function selectDifferentDrivers(drivers, func) {
+    return func(drivers);
+}
+
